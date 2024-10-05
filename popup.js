03ec2435ee46fe1ff12c4e71a431337a7452ce92
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-10-06 00:01:35 +0100

    popup.js logic complete

--- popup.js
+++ popup.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function(){
 // STATE VARIABLE
 let providerURL = 'https://shape-sepolia.g.alchemy.com/v2/SyfDzrjp_igUQ-lhN3_jpksTLA48Feik';
 
-let provider;
+// let provider;
 let privateKey;
 let address;
 
@@ -58,50 +58,389 @@ function handler(){
 
   const amount = document.getElementById("amount").value;
   const address = document.getElementById("address").value;
+
+  const private_key = "dvdkbsnjnnfksn";
+  const testAccount = "fbbfiaisdsuidiuvbf";
+
+  // PROVIDER
+  const provider = new ethers.providers.JsonRpcProvider(providerURL);
+
+  let wallet = new ethers.Wallet(private_key, provider);
+
+  const tx = {
+    to: address,
+    value: ethers.utils.parseEther(amount),
+  };
+
+  let a = document.getElementById("link");
+  a.href = "somelink-url";
+
+  wallet.sendTransaction(tx).then((txOb) => {
+    console.log("txHash:", txObj.hash);
+
+    document.getElementById("transfer_center").style.display = "none";
+    const a = document.getElementById("link");
+
+    document.getElementById("link").style.display = "block";
+  });
 };
 
-function checkbalance(){};
+function checkbalance(){
+  const provider = new ethers.providers.JsonRpcProvider(providerURL);
 
-function getOpenNetwork(){};
+  provider.getBalance(address).then((balance) => {
+    const balanceInEth = ethers.utils.formatEther(balance);
 
-function getSelectedNetwork(){};
+    document.getElementById(
+      "accountBalance"
+    ).innerHTML = `${balanceInEth} MATIC`;
 
-function setNetwork(){};
+    document.getElementById("userAddress").innerHTML = `${address.slice(
+      0,
+      15
+    )}...`;
+  });
+};
 
-function loginUser() {};
+function getOpenNetwork(){
+  document.getElementById("network").style.display = "block";
+};
 
-function createUser() {};
+function getSelectedNetwork(e){
+  const element = document.getElementById("selected_network");
+  element.innerHTML = e.target.innerHTML;
 
-function openCreate(){};
+  if (e.target.innerHTML === "Ethereum Mainnet") {
+    providerURL = "https://eth-mainnet.g.alchemy.com/v2/SyfDzrjp_igUQ-lhN3_jpksTLA48Feik"
+    document.getElementById("network").style.display = "none";
+  } else if (e.target.innerHTML === "Polygon Mainnet") {
+    providerURL = "https://rpc.ankr.com/polygon";
+    document.getElementById("network").style.display = "none";
+  } else if (e.target.innerHTML == "Polygon Mumbai") {
+    providerURL = "https://polygon-mumbai.g.alchemy.com/v2/demo";
+    document.getElementById("network").style.display = "none";
+  } else if (e.target.innerHTML == "Goerli test network") {
+    providerURL = "https://rpc.ankr.com/eth_goerli";
+    document.getElementById("network").style.display = "none";
+  } else if (e.target.innerHTML == "Sepolia test network") {
+    providerURL = "https://shape-sepolia.g.alchemy.com/v2/SyfDzrjp_igUQ-lhN3_jpksTLA48Feik";
+    document.getElementById("network").style.display = "none";
+  }
 
-function signUp() {};
+  console.log(providerURL);
+};
 
-function login(){};
+function setNetwork(){
+  document.getElementById("network").style.display = "none";
+};
 
-function logout(){};
+function loginUser(){
+  document.getElementById("createAccount").style.display = "none";
+  document.getElementById("LoginUser").style.display = "block";
+};
 
-function openTransfer(){};
+function createUser() {
+  document.getElementById("createAccount").style.display = "block";
+  document.getElementById("LoginUser").style.display = "none";
+};
 
-function goBack(){};
+function openCreate(){
+  document.getElementById("createAccount").style.display = "none";
+  document.getElementById("create_popup").style.display = "block";
+};
 
-function openImport(){};
+function signUp() {
+  const name = document.getElementById("sign_up_name").value;
+  const email = document.getElementById("sign_up_email").value;
+  const password = document.getElementById("sign_up_password").value;
+  const passwordConfirm = document.getElementById("sign_up_passwordConfirm").value;
 
-function importGoBack(){};
+  document.getElementById("field").style.display = "none";
+  document.getElementById("center").style.display = "block";
 
-function openActivity(){};
+  const wallet = ethers.Wallet.createRandom();
 
-function goHomePage(){};
+  if(wallet.address) {
+    console.log(wallet)
 
-function openImportModel(){};
+    // API CALL
+    const url = "http://localhost:3000/api/v1/user/signup";
 
-function closeImportModel(){};
+    const data = {
+      name,
+      email,
+      password,
+      passwordConfirm,
+      address: wallet.address,
+      private_key: wallet.privateKey,
+      mnemonic: wallet.mnemonic.phrase
+    }
 
-function addToken(){};
+    fetch(url, {
+      method: "POST",
+      handlers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        document.getElementById("createdAddress").innerHTML = wallet.address;
+        document.getElementById("createdPrivateKey").innerHTML = wallet.privateKey;
+        document.getElementById("createdMnemonic").innerHTML = wallet.mnemonic.phrase;
+        document.getElementById("center").style.display = "none";
+        document.getElementById("accountData").style.display = "block";
+        document.getElementById("sign_up").innerHTML = "none";
 
-function addAccount(){};
+        const userWallet = {
+          address: wallet.address,
+          private_key: wallet.privateKey,
+          mnemonic: wallet.mnemonic.phrase
+        };
 
-function myFunction(){};
+        const jsonObj = JSON.stringify(userWallet);
+        localStorage.setItem("userWallet", jsonObj);
 
-function copyAddress(){};
+        document.getElementById("goHomePage").style.display = "block";
+        window.location.reload();
+      }).catch((error) => {
+        console.log("Error:", error)
+      });
+  }
+};
 
-function changeAccount(){};
+function login(){
+  document.getElementById("login_form").style.display = "none";
+  document.getElementById("center").style.display = "block";
+
+  const email = document.getElementById("login_email").value;
+  const password = document.getElementById("login_password").value;
+
+  // API CALL
+  const url = "http://localhost:3000/api/v1/user/login";
+  const data = {
+    email,
+    password
+  }
+
+  fetch(url, {
+    method: "POST",
+    handlers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+    const userWallet = {
+      address: result.data.user.address,
+      private_key: result.data.user.private_key,
+      mnemonic: result.data.user.mnemonic
+    };
+
+    const jsonObj = JSON.stringify(userWallet);
+    localStorage.setItem("userWallet", jsonObj);
+    window.location.reload();
+  }).catch((error) => {
+    console(error);
+  });
+};
+
+function logout(){
+  localStorage.removeItem("userWallet");
+  window.location.reload();
+};
+
+function openTransfer(){
+  document.getElementById("transfer_from").style.display = "block";
+  document.getElementById("home").style.display = "none";
+};
+
+function goBack(){
+  document.getElementById("transfer_from").style.display = "none";
+  document.getElementById("home").style.display = "block";
+};
+
+function openImport(){
+  document.getElementById("import_token").style.display = "block";
+  document.getElementById("home").style.display = "none";
+};
+
+function importGoBack(){
+  document.getElementById("import_token").style.display = "none";
+  document.getElementById("home").style.display = "block";
+};
+
+function openActivity(){
+  document.getElementById("activity").style.display = "block";
+  document.getElementById("assets").style.display = "none";
+};
+
+function openAssets() {
+  document.getElementById("activity").style.display = "none";
+  document.getElementById("assets").style.display = "block";
+}
+
+function goHomePage(){
+  document.getElementById("create_popUp").style.display = "none";
+  document.getElementById("home").style.display = "block";
+};
+
+function openImportModel(){
+  document.getElementById("import_account").style.display = "block";
+  document.getElementById("home").style.display = "none";
+};
+
+function closeImportModel(){
+  document.getElementById("import_account").style.display = "none";
+  document.getElementById("home").style.display = "block";
+};
+
+function addToken(){
+  const address = document.getElementById("token_Address").value;
+  const name = document.getElementById("token_name").value;
+  const symbol = document.getElementById("token_symbol").value;
+
+  // API CALL
+  const url = "http://localhost:3000/api/v1/tokens/createtoken";
+  const data = {
+    name,
+    address,
+    symbol,
+  };
+
+  fetch(url, {
+    method: "POST",
+    handlers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+    window.location.reload();
+  }).catch((error) => {
+    console("ERROR", error);
+  });
+};
+
+function addAccount(){
+  const privateKey = document.getElementById("add_account_private_key").value;
+
+  const provider = new ethers.providers.JsonRpcProvider(providerURL);
+
+  let wallet = new ethers.Wallet(privateKey, provider);
+
+  console.log(wallet);
+
+  const url = "http://localhost:3000/api/v1/account/createaccount";
+
+  const data = {
+    privateKey,
+    address: wallet.address
+  };
+
+  fetch(url, {
+    method: "POST",
+    handlers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console(error);
+  });
+};
+
+function myFunction(){
+  const str = localStorage.getItem("userWallet");
+  const parsedObj = JSON.parse(str);
+
+  if (parsedObj.address) {
+    document.getElementById("LoginUser").style.display = "none";
+    document.getElementById("home").style.display = "block";
+
+    privateKey = parsedObj.private_key;
+    address = parsedObj.address;
+
+    checkbalance(parsedObj.address);
+  }
+
+  const tokenRender = document.querySelector(".assets");
+  const accountRender = document.querySelector(".accountList");
+  const url = "http://localhost:3000/api/v1/tokens/alltoken";
+
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(result);
+    let elements = "";
+
+    data.data.tokens.map((token) =>
+    (elements += `
+    <div class="assets_item>
+    <img class="assets_item_img"
+    src=".assets/theblockchaincoders.png"
+    alt=""
+    />
+
+    <span> ${token.address.slice(0, 15)}...</span>
+    <span>${token.symbol}</span>
+    </div>
+    `)
+    );
+    tokenRender.innerHTML = elements;
+  }).catch((error) => {
+    console(error);
+  });
+
+  fetch("http://localhost:3000/api/v1/tokens/allaccount").then((response) => response.json()).then((data)=> {
+    let accounts = "";
+
+    data.data.accounts.map((account, i) => 
+    (account += `
+    <div class="lists">
+    <p>${i + 1}</p>
+    <p class="accountValue" data-address=${
+      account.address
+    } data-privateKey=${account.privateKey}>${account.address.slice(0, 25)}...</p>
+    </div>
+    `))
+    accountRender.innerHTML = accounts;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  console.log(privateKey);
+};
+
+function copyAddress(){
+  navigator.clipboard.writeText(address);
+};
+
+function changeAccount(){
+  const data = document.querySelector(".accountValue");
+  const address = data.getAttribute("data-address");
+  const privateKey = data.getAttribute("data-privateKey");
+
+  console.log(privateKey, address);
+
+  const userWallet = {
+    address,
+    private_key: privateKey,
+    mnemonic: "Changed",
+  };
+
+  const jsonObj = JSON.stringify(userWallet);
+  localStorage.setItem("userWallet", jsonObj);
+
+  window.location.reload();
+};
+
+window.onload = myFunction;
